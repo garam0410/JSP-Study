a34b7eaedc2b84586f3d93c8da7119c697308d7e
Garam Kim <0410garam@naver.com>
2020-02-12 16:13:31 +0900

    JSP Forwarding

---
+++ chapter26/src/com/jsp/ex/RequestObj.java
@@ -0,0 +1,50 @@
+package com.jsp.ex;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestObj
+ */
+@WebServlet("/RequestObj")
+public class RequestObj extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestObj() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+
+	public void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("actionDo");
+		
+		request.setAttribute("id", "abcde");
+		request.setAttribute("pw", "12345");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/dispatcherjsp.jsp");
+		dispatcher.forward(request, response);
+	}
+}
